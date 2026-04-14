3155422d9597cebaba4d87e75781a27463162535
yg1112 <gaoyukun1205@gmail.com>
2026-04-13 23:27:39 -0700

    Update SHA256 for final CLIInstaller fix build

--- Formula/mint-cli.rb
+++ Formula/mint-cli.rb
@@ -2,7 +2,7 @@ class MintCli < Formula
   desc "Storage intelligence CLI for macOS - scan, clean, and monitor disk usage"
   homepage "https://mint.dzgapp.com"
   url "https://github.com/yg1112/mint-releases/releases/download/v1.0.1/mint-cli-1.0.1-macOS.tar.gz"
-  sha256 "290ce5e59e5a7de37c7fec231b2a20f28b734c688aa8cc54be1a67f0382c2816"
+  sha256 "9b9363f06d20e4b909127727a47c67fad8b5bd0605afe10117e6e34a6d127add"
   version "1.0.1"
   license :cannot_represent
 
